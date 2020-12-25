004558d685bd2ad9f7737274e72d3f16f743fbcb
gzstyp <444141300@qq.com>
2020-12-25 21:28:22 +0800

    拼音首字母

---
+++ common/src/main/java/com/fwtai/tool/ToolChinese.java
@@ -0,0 +1,62 @@
+package com.fwtai.tool;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+/**处理汉字转字母
+ * @作者 田应平
+ * @版本 v1.0
+ * @创建时间 2020-12-25 21:09
+ * @QQ号码 444141300
+ * @Email service@dwlai.com
+ * @官网 http://www.fwtai.com
+ */
+public final class ToolChinese{
+
+    /**
+     * 得到 汉字的全拼
+     * @param src 中文字符串
+     * @return
+    */
+    public static String getPingYinAll(final String src) {
+        final HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+        format.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        format.setVCharType(HanyuPinyinVCharType.WITH_V);
+        final StringBuilder sb = new StringBuilder();
+        final char[] srcArray = src.toCharArray();
+        try {
+            for (int i = 0; i < srcArray.length; i++) {
+                // 判断是否为汉字字符
+                if (java.lang.Character.toString(srcArray[i]).matches("[\\u4E00-\\u9FA5]+")) {
+                    final String[] targetArray = PinyinHelper.toHanyuPinyinStringArray(srcArray[i], format);
+                    sb.append(targetArray[0]);
+                } else {
+                    sb.append(java.lang.Character.toString(srcArray[i]));
+                }
+            }
+            return sb.toString();
+        } catch (BadHanyuPinyinOutputFormatCombination e1) {
+            e1.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public static String getPinYinHeadChar(final String str) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            final char word = str.charAt(i);
+            final String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
+            if (pinyinArray != null) {
+                sb.append(pinyinArray[0].charAt(0));
+            } else {
+                sb.append(word);
+            }
+        }
+        return sb.toString();
+    }
+}
