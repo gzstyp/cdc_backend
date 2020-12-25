ff868a5a4b583aba26eb784b19b533669dc2e54f
gzstyp <444141300@qq.com>
2020-12-25 12:44:28 +0800

    不能大于采样人数

--- service/src/main/java/com/fwtai/service/api/ApiCrowdTotalService.java
+++ service/src/main/java/com/fwtai/service/api/ApiCrowdTotalService.java
@@ -45,11 +45,14 @@ public class ApiCrowdTotalService{
         if(validate != null)return validate;
         final String validateInteger = ToolClient.validateInteger(formData,p_area_level,p_detection_total,p_masculine_total,p_sampling_total);
         if(validateInteger != null)return validateInteger;
+        final int sampling = formData.getInteger(p_sampling_total);
         final int detection = formData.getInteger(p_detection_total);
         final int masculine = formData.getInteger(p_masculine_total);
-        final int sampling = formData.getInteger(p_sampling_total);
-        if((detection + masculine) > sampling){
-            return ToolClient.createJsonFail("检测数和阳性数大于采样人数");
+        if(detection > sampling){
+            return ToolClient.createJsonFail("检测人数大于采样人数");
+        }
+        if(masculine > sampling){
+            return ToolClient.createJsonFail("阳性人数大于采样人数");
         }
         final String kid = ToolString.getIdsChar32();
         formData.put("kid",kid);
@@ -87,11 +90,14 @@ public class ApiCrowdTotalService{
         if(validate != null)return validate;
         final String validateInteger = ToolClient.validateInteger(formData,p_area_level,p_detection_total,p_masculine_total,p_sampling_total);
         if(validateInteger != null)return validateInteger;
+        final int sampling = formData.getInteger(p_sampling_total);
         final int detection = formData.getInteger(p_detection_total);
         final int masculine = formData.getInteger(p_masculine_total);
-        final int sampling = formData.getInteger(p_sampling_total);
-        if((detection + masculine) > sampling){
-            return ToolClient.createJsonFail("检测数和阳性数大于采样人数");
+        if(detection > sampling){
+            return ToolClient.createJsonFail("检测人数大于采样人数");
+        }
+        if(masculine > sampling){
+            return ToolClient.createJsonFail("阳性人数大于采样人数");
         }
         final String exist_key = apiCrowdTotalDao.queryExistById(formData.getString(p_kid));
         if(exist_key == null){
