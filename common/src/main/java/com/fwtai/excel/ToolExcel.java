991a6e50fb2d42641c9df96629a5517b008dd2c7
gzstyp <444141300@qq.com>
2020-12-31 22:49:18 +0800

    生成文件

--- common/src/main/java/com/fwtai/excel/ToolExcel.java
+++ common/src/main/java/com/fwtai/excel/ToolExcel.java
@@ -2,9 +2,12 @@ package com.fwtai.excel;
 
 import com.fwtai.bean.PageFormData;
 import com.fwtai.tool.ToolString;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
@@ -27,6 +30,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
@@ -639,10 +643,52 @@ public final class ToolExcel{
 
             for (int j = 0; j < 10; j++){
                 final Cell cell = rowData.createCell(j);
+                final XSSFCellStyle cellStyle = wb.createCellStyle();
+                cellStyle.setWrapText(true);
+                cell.setCellStyle(cellStyle);
                 cell.setCellValue("我是内容啊");
             }
         }
         return wb;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //创建一个Excel
+        final Workbook wb = new HSSFWorkbook();
+        //创建一个输出流对象,以便将创建好的Excel写入文件
+        final FileOutputStream fileOut = new FileOutputStream("C:\\WORKBOOK.xls");
+        //
+        final CreationHelper createHelper = wb.getCreationHelper();
+        //创建一个sheet
+        final Sheet sheet = wb.createSheet("附表");
+        //合并一个单元格,这个地方会在下面用图介绍
+        sheet.addMergedRegion(new CellRangeAddress(
+            0, //first row (0-based)
+            10, //last row  (0-based)
+            0, //first column (0-based)
+            1  //last column  (0-based)
+        ));
+
+        //创建一个行对象,下标从0开始
+        Row row1=sheet.createRow(0);
+        //创建一个样式
+        CellStyle cellStyle1 = wb.createCellStyle();
+        //设置左右对齐居中
+        cellStyle1.setAlignment(HorizontalAlignment.CENTER_SELECTION);
+        //垂直对其居中
+        cellStyle1.setVerticalAlignment(VerticalAlignment.CENTER);
+        //设置true让Cell中的内容以多行显示
+        cellStyle1.setWrapText(true);
+        //创建一个单元格,单元格下标从0开始
+        Cell c1=row1.createCell(0);
+        //创建富文本,如果你想每一行显示一个字体那么就按照我下面的写,如果想多个显示在一行,那么去掉他们之间的\r\n就好了
+        c1.setCellValue(createHelper.createRichTextString("合\r\n并\r\n后\r\n选\r\n择\r\n我\r\n，\r\n这\r\n只\r\n值\r\n！"));
+        //设置样式
+        c1.setCellStyle(cellStyle1);
+        //写入文件
+        wb.write(fileOut);
+        //关闭输出流
+        fileOut.close();
     }
 
     //合并单元格,指定 4 个参数，起始行，结束行，起始列，结束列。这个区域将被合并;更复杂些的，需要自己计算好行与列即可
