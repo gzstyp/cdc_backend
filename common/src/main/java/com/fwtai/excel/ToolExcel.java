a0b2648b47a080075116ff6b252cca9e2e3c35fc
gzstyp <444141300@qq.com>
2021-01-03 15:40:54 +0800

    合计

--- common/src/main/java/com/fwtai/excel/ToolExcel.java
+++ common/src/main/java/com/fwtai/excel/ToolExcel.java
@@ -645,12 +645,13 @@ public final class ToolExcel{
             final int length = sampling.split(",").length;
             final Cell cell = crowdRow.createCell(crowdCell);//第1格子
             cell.setCellValue(crowdName);
+            final int len = length * 3;
             if(x == tabsTotal -1){//处理最后一个人群类型时附加总计
-                cellRangeAddress(sheet,1,1,crowdCell,(crowdCell + length * 3 - 1) + 6);//附加总计;6 = 3 + 3,其中的一个3是合计，其中一个3是总计
-                crowdCell = crowdCell + length * 3 + 6;
+                cellRangeAddress(sheet,1,1,crowdCell,(crowdCell + len - 1) + 6);//附加总计;6 = 3 + 3,其中的一个3是合计，其中一个3是总计
+                crowdCell = crowdCell + len + 6;
             }else{
-                cellRangeAddress(sheet,1,1,crowdCell,(crowdCell + length * 3 - 1) + 3);//附加合计,一个3是合计
-                crowdCell = crowdCell + length * 3 + 3;
+                cellRangeAddress(sheet,1,1,crowdCell,(crowdCell + len - 1) + 3);//附加合计,一个3是合计
+                crowdCell = crowdCell + len + 3;
                 for(int z = 0; z < length; z++){
                     final Cell _cell = typeRow.createCell(z*3);
                     cellRangeAddress(sheet,2,2,z*3,z*3+2);
