7d450b0cfbc461ac5ec9b055c97fb76a4c9a251e
gzstyp <444141300@qq.com>
2021-01-05 20:10:52 +0800

    创建表格并填充数据

--- common/src/main/java/com/fwtai/poi/ToolWord.java
+++ common/src/main/java/com/fwtai/poi/ToolWord.java
@@ -175,35 +175,67 @@ public final class ToolWord{
         singleRow(doc,"表5  不同从业人员监测情况",14,ParagraphAlignment.CENTER,true,false);
 
         //final OptionalInt optMax = data.stream().mapToInt(HashMap::size).max();//简化代码
-        final OptionalInt optMax = listEmployee.stream().mapToInt(value -> {
-            final String profession = (String) value.get("profession");
-            final String[] split = profession.split(",");
+        final OptionalInt optMaxEmployee = listEmployee.stream().mapToInt(value -> {
+            final String arrs = (String) value.get("profession");
+            final String[] split = arrs.split(",");
             return split.length;
         }).max();
-        final int cols = optMax.getAsInt();//获取最大值
+        final int colsEmployee = optMaxEmployee.getAsInt();//获取最大值
+
+        final OptionalInt optMaxSiteType = listSiteType.stream().mapToInt(value -> {
+            final String arrs = (String) value.get("site_type");
+            final String[] split = arrs.split(",");
+            return split.length;
+        }).max();
+
+        final int colsSiteType = optMaxSiteType.getAsInt();
+
+        createTable(doc,listEmployee,colsEmployee,"profession","name","地区","profession_total","合计");
+
+        newLine(doc);//换行
+
+        createTable(doc,listSiteType,colsSiteType,"site_type","area","地区","type_total","合计");
+
+        downloadWord(doc,fileName,response);
+    }
+
+    /**
+     * 创建表格并填充数据
+     * @param doc word文档对象
+     * @param listData 数据
+     * @param cols 最大个数作为表头的列个数
+     * @param horizontalKey 从 listData 里分组[水平横向方向的字段]的key,一般指的是类型或类别的count(xxx)字段
+     * @param startVerticalKey 从 listData 里分组[垂直竖向方向的字段]获取作为数据行的第1行的第1列的key,一般是 group by xxx字段
+     * @param startColumnText 表头的第1个单元格的名称文本内容,如:地区
+     * @param totalKey 一般是指 count(xxx) as xxx_total
+     * @param endColumnText 表头的第最后1个单元格的名称文本内容,如:合计
+     * @作者 田应平
+     * @QQ 444141300
+     * @创建时间 2021/1/5 19:18
+    */
+    private static void createTable(final XWPFDocument doc,final List<HashMap<String,Object>> listData,final int cols,final String horizontalKey,final String startVerticalKey,final String startColumnText,final String totalKey,final String endColumnText){
+        final XWPFTable table = doc.createTable();
         HashMap<String,Object> _map_ = new HashMap<>(cols);
-        for(int i = 0; i < listEmployee.size(); i++){
-            final String[] professions = ((String) listEmployee.get(i).get("profession")).split(",");
+        for(int i = 0; i < listData.size(); i++){
+            final String[] professions = ((String) listData.get(i).get(horizontalKey)).split(",");
             if(cols == professions.length){
-                _map_ = listEmployee.get(i);
+                _map_ = listData.get(i);
                 break;
             }
         }
-        final XWPFTable table = doc.createTable();
         final XWPFTableRow titleRow = table.getRow(0);//创建的的一行一列的表格，获取第一行
-        initTableTitle(titleRow,((String)_map_.get("profession")).split(","),"地区","合计");
-        for(int i = 0; i < listEmployee.size(); i++){
-            final HashMap<String,Object> map = listEmployee.get(i);
+        initTableTitle(titleRow,((String)_map_.get(horizontalKey)).split(","),startColumnText,endColumnText);
+        for(int i = 0; i < listData.size(); i++){
+            final HashMap<String,Object> map = listData.get(i);
             final XWPFTableRow row = table.createRow();
-            final String[] profession_totals = ((String) map.get("profession_total")).split(",");
+            final String[] profession_totals = ((String) map.get(totalKey)).split(",");
             long itemTotal = 0;
             for(int x = 0; x < profession_totals.length; x++){
                 itemTotal += Long.parseLong(profession_totals[x]);
             }
-            final String item = ((String) map.get("name")).split(",")[0];
+            final String item = ((String) map.get(startVerticalKey)).split(",")[0];
             fillRowData(row,profession_totals,item,cols+1,String.valueOf(itemTotal));
         }
-        downloadWord(doc,fileName,response);
     }
 
     /**
@@ -249,6 +281,13 @@ public final class ToolWord{
             run.addCarriageReturn();
         if(bold)
             run.setBold(true);
+    }
+
+    /**换行*/
+    protected static void newLine(final XWPFDocument doc){
+        final XWPFParagraph paragraph = doc.createParagraph();
+        final XWPFRun run = paragraph.createRun();
+        run.addCarriageReturn();
     }
 
     /**
