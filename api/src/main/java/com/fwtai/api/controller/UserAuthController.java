45045c619c55d196d230abc85c54dfae368d7f51
gzstyp <444141300@qq.com>
2021-03-06 13:56:43 +0800

    小程序|移动端登录

--- api/src/main/java/com/fwtai/api/controller/UserAuthController.java
+++ api/src/main/java/com/fwtai/api/controller/UserAuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 小程序|移动端登录,这个仅能包含一个登录方法即可
+ * 小程序|移动端登录,这个仅能包含一个登录方法
  * @作者 田应平
  * @版本 v1.0
  * @创建时间 2020-07-17 15:08
@@ -23,12 +23,11 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class UserAuthController{
 
-    //实际上是不走这个方法,但是后走登录验证的方法
-    @ApiOperation(value = "小程序|移动端登录", notes = "要求输入账号(用户名)、登录密码、type类型为3")
+    @ApiOperation(value = "小程序|移动端登录", notes = "要求输入账号(用户名)、登录密码、type登录类型1为android;2为ios;3为小程序")
     @ApiImplicitParams({
       @ApiImplicitParam(name = "username", value = "登录账号(用户名|手机号)", dataType = "String", paramType = "query", required = true),
       @ApiImplicitParam(name = "password", value = "登录密码", dataType = "String", paramType = "query", required = true),
-      @ApiImplicitParam(name = "type", value = "登录密码", dataType = "int", example = "3",paramType = "query", required = true)
+      @ApiImplicitParam(name = "type", value = "登录类型: 1为android;2为ios;3为小程序", dataType = "int",paramType = "query", required = true,defaultValue = "1")
     })
     @PostMapping(value = "/login")
     public void edit(final HttpServletResponse response){
